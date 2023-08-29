2c41d679566f67183686a58e9aaa1afe2b01b2fa
bahaazaki <bahaa.zaki@damen.com.eg>
2023-08-29 11:54:25 +0300

    Storing Messages With MongoDB In A Database

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
 	if (req.method === "POST") {
 		const { email, name, message } = req.body;
 		console.group();
@@ -25,9 +27,28 @@ function handler(req, res) {
 			message,
 		};
 
-		console.log({ newMessage });
+		let client;
+		try {
+			client = await MongoClient.connect(
+				"mongodb+srv://zoka:H2OHXDKOFP18jfDg@cluster0.yofqh5m.mongodb.net/next-blog?retryWrites=true&w=majority"
+			);
+		} catch (error) {
+			res.status(500).json({ message: "Could not connect to database!" });
+			return;
+		}
 
-		res.status(201).json({ message: "You message has been received" });
+		const db = client.db();
+
+		try {
+			const result = await db.collection("messages").insertOne(newMessage);
+			newMessage.id = result.insertedId;
+		} catch (error) {
+			res.status(500).json({ message: "Storing message failed!" });
+			client.close();
+			return;
+		}
+
+		res.status(201).json({ message: "Your message has been received.", message: newMessage });
 	}
 }
 
